5ce0099cfe3775a42a959472f7e0ecf6f6c4c6ae
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-03-12 16:13:25 +0530

    Add installation of tailwind-css and other stuff

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <h1 className="text-5xl text-blue-500 font-bold">
+        Portfolio Setup Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
